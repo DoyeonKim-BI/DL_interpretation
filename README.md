00e8b386e0d38f14cab38b84e0d5d5ab656c99f8
DoyeonKim-BI[2~ <rlaehdus0918@snu.ac.kr>
2021-09-29 16:13:04 +0900

    update README and code

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Collection and implementation of tensorflow keras codes for deep learning model interpretation methods (mostly, CNN)  
 Major notes:
 - TensorFlow version: 2.2.0
-- Enabled structured input
-- Flexible for 1D images (e.g. nucleotide sequence)
+- Applicable to structured inputs
+- Applicable to 1D images (e.g. nucleotide sequence)
---
+++ method_zoo.py
@@ -0,0 +1,223 @@
+import sys, os, copy, pickle, math,time,re, itertools
+import pandas as pd
+import numpy as np
+import tensorflow as tf #ver.2.2.0
+from scipy import stats
+
+## Collection of basic methods for deep learning model interpretation
+
+####################################################################################
+## Saliency map, Grad*Input and Integrated gradients                              ##
+## Modified A_K_Nain code (https://keras.io/examples/vision/integrated_gradients/)##
+## enabled structured inputs                                                      ##
+####################################################################################
+def get_gradients(inputs, model):
+    """Computes the gradients of outputs w.r.t input.
+    Args:
+        inputs: np.array(tensor of (1,*img)) or dictionary of array (structured input)
+        model:  my model
+    Returns:
+        Gradients of the predictions w.r.t input
+    """
+    
+    if isinstance(inputs, dict): #structured input
+        inputs = {key: tf.cast(inputs[key], tf.float32) for key in inputs}
+    else:
+        inputs = tf.cast(inputs, tf.float32)
+    with tf.GradientTape() as tape:
+        tape.watch(inputs)
+        preds = model(inputs)
+    
+    grads = tape.gradient(preds, inputs)
+    return grads # same structure as inputs
+
+## TODO: Saliency map (Simonyan et al., https://arxiv.org/pdf/1312.6034.pdf)
+
+## Grad*Input (Baehrens et al., https://jmlr.org/papers/volume11/baehrens10a/baehrens10a.pdf)
+def get_grad_input(inputs, model):
+    """
+    Args:
+        inputs: np.array(tensor of (1,*img)) or dictionary of array (structured input)
+        model:  my model
+    Returns:
+        Grad*Input
+    """
+    grads = get_gradients(inputs,model)
+    if isinstance(inputs, dict): #structured input
+        grad_input = {grads[key]*inputs[key] for key in inputs.keys()}
+    else:
+        grad_input = grads * inputs
+    return grad_input
+
+## Integrated gradients (Sundatatajan et al., http://proceedings.mlr.press/v70/sundararajan17a/sundararajan17a.pdf)
+def get_integrated_gradients(inputs, model, baseline=None, num_steps=50):
+    """Computes Integrated Gradients for a predicted label.
+    Args:
+        inputs: np.array(tensor of (1,*img)) or dictionary of array (structured input)
+        model:  my model 
+        baseline (same format as inputs): The baseline image to start with for interpolation
+        num_steps: Number of interpolation steps between the baseline
+            and the input used in the computation of integrated gradients. These
+            steps along determine the integral approximation error. By default,
+            num_steps is set to 50.
+
+    Returns:
+        Integrated gradients w.r.t inputs
+    """
+    # If baseline is not provided, start with a black image (all-zeros)
+    # having same size as the input image.
+    if isinstance(inputs, dict): #structured input
+        inputs = {key: inputs[key].astype(np.float32) for key in inputs}
+        if baseline is None:
+            baseline = {key: np.zeros(inputs[key].shape[1:]).astype(np.float32) for key in inputs}
+        else:
+            baseline = {key: baseline[key].astype(np.float32) for key in baseline}
+        
+        # 1. Do interpolation.
+        interpolated_inputs = {}
+        for key,img in inputs.items():
+            interpolated_inputs[key] = [baseline[key] + (step/num_steps)*(img-baseline[key]) for step in range(num_steps+1)]
+        interpolated_inputs = {key: np.concatenate(interpolated_img, axis=0).astype(np.float32) \
+                               for key, interpolated_img in interpolated_inputs.items()}
+        
+        # 2. Get the gradients.
+        grads = {key:[] for key in interpolated_inputs.keys()}
+        for i in range(num_steps+1):
+            tmp_itp_inputs = {key:tf.expand_dims(img[i],axis=0) for key,img in interpolated_inputs.items()}
+            grad_dict = get_gradients(tmp_itp_inputs, model)
+            for key,grad in grad_dict.items():
+                grads[key].append(grad[0])
+        grads = {key:tf.convert_to_tensor(grad, dtype=tf.float32) for key,grad in grads.items()}
+        
+        # 3. Approximate the integral using the trapezoidal rule.
+        grads = {key: (grad[:-1] + grad[1:]) / 2.0 for key,grad in grads.items()}
+        avg_grads = {key: tf.reduce_mean(grad, axis=0) for key,grad in grads.items()} 
+
+        # 4. Calculate integrated gradients.
+        integrated_grads = {key:(img-baseline[key])*avg_grads[key] for key,img in inputs.items()}
+
+    else:
+        inputs = inputs.astype(np.float32)
+        if baseline is None:
+            baseline = np.zeros(inputs.shape[1:]).astype(np.float32)
+        else:
+            baseline = baseline.astype(np.float32)
+            
+        # 1. Do interpolation.
+        interpolated_inputs = [baseline + (step/num_steps)*(inputs-baseline) for step in range(num_steps+1)]
+        interpolated_inputs = np.concatenate(interpolated_inputs,axis=0).astype(np.float32)
+        
+        # 2. Get the gradients.
+        grads = []
+        for i, img in enumerate(interpolated_inputs):
+            img = tf.expand_dims(img, axis=0)
+            grad = get_gradients(img, top_pred_idx=top_pred_idx)
+            grads.append(grad[0])
+        grads = tf.convert_to_tensor(grads, dtype=tf.float32)
+        
+        # 3. Approximate the integral using the trapezoidal rule.
+        rads = (grads[:-1] + grads[1:]) / 2.0
+        avg_grads = tf.reduce_mean(grads, axis=0)
+
+        # 4. Calculate integrated gradients.
+        integrated_grads = (img_input - baseline) * avg_grads
+    
+    return integrated_grads
+
+## Modified https://jacobgil.github.io/deeplearning/class-activation-maps
+## Enabled regression output interpretation
+def get_cam(inputs, model, final_conv_layer_name='', output_layer_name = '',
+            pred_index=None, apply_clip = "no"):
+    """
+    params:
+    -------
+    inputs: np.array(tensor of (1,*img)) (structured input: nonavailable)
+    model:  my model (CAUTION: the last two layers should be GAP->Dense)
+    final_conv_layer_name: layer name to visualize (should be the convolution layer before GAP)
+    apply_clip: ['negative','positive','no'] When you want to focus on positive/negative contribution only
+        negative-> negative relu / no-> do not apply anything (default) / positive-> relu
+    return:
+    cam: CAM heatmap
+    """
+    class_weights      = model.layers[-1].get_weights()[0]
+    final_conv_layer   = model.get_layer(final_conv_layer_name)
+    output_layer       = model.get_layer(output_layer_name).output
+    intermediate_model = tf.keras.models.Model([model.get_input_at(0)], [final_conv_layer.output, output_layer])
+    conv_outputs, predictions = intermediate_model([inputs])
+    output = np.moveaxis(conv_outputs[0],-1,0) #for convenience, move channel axis into the first
+    
+    cam    = np.zeros(dtype = np.float32, shape = output.shape[:-1])
+    #if pred_index is None: RAM (regression activation map)
+    if not(pred_index is None): #CAM
+        class_weights = class_weights[:, pred_index]
+        
+    assert len(class_weights) == output.shape[0]
+    for i, w in enumerate(class_weights):
+        cam += w * output[i]
+                
+    assert apply_clip in ['negative','positive','no'], \
+    "apply_clip should be one of: negative (-inf,0], positive [0,inf), no"
+    if   apply_clip == 'negative':
+        cam = np.clip(cam, a_min=None, a_max=0)
+    elif apply_clip == 'positive':
+        cam = np.clip(cam, a_min=0,    a_max=None)
+    return cam
+
+## Grad-CAM (Gradient-weighted Class Activation Map)
+## Modified fchollet code (https://keras.io/examples/vision/grad_cam/),
+## Enabled structured input, and enabled regression output interpretation
+def get_grad_cam(inputs, model, activation_layer_name='',output_layer_name='', pred_index=None,
+                 key_list = [], apply_clip = "no"):
+    """
+    params:
+    -------
+    inputs: np.array(tensor of (1,*img)) or dictionary of array (structured input)
+    model: my model
+    activation_layer_name: layer name to visualize
+    apply_clip: ['negative','positive','no'] When you want to focus on positive/negative contribution only
+        negative-> negative relu / no-> do not apply anything (default) / positive-> relu
+
+    return:
+    grad_cam: Grad-CAM heatmap
+    """
+    if type(inputs) == dict:
+        assert all([(key in inputs.keys()) for key in key_list])
+        converted_inputs = [inputs[key] for key in key_list]
+    else:
+        converted_inputs = inputs
+        
+    activation_layer  = model.get_layer(activation_layer_name)
+    cam_rank = len(activation_layer.output.shape)-1
+    grad_output_layer = model.get_layer(output_layer_name).output
+    
+    #if pred_index is None: RAM (regression activation map)
+    if not(pred_index is None): #CAM
+        grad_output_layer = grad_output_layer[:,pred_index]
+    grad_model = tf.keras.models.Model([model.get_input_at(0)], [activation_layer.output, grad_output_layer])
+    
+    with tf.GradientTape() as tape:
+        conv_outputs, predictions = grad_model([converted_inputs])
+        loss = predictions
+    
+    output = conv_outputs[0]
+    
+    grads = tape.gradient(loss, conv_outputs)[0]
+
+    axis         = tuple([i for i in range(cam_rank-1)])
+    pooled_grads = tf.reduce_mean(grads, axis=axis)
+    
+    grad_cam = output @ pooled_grads[..., tf.newaxis]
+    grad_cam = tf.squeeze(grad_cam).numpy()
+    
+    #When you want to focus on positive/negative contribution only
+    assert apply_clip in ['negative','positive','no'], \
+    "apply_clip should be one of: negative (-inf,0], positive [0,inf), no"
+    if   apply_clip == 'negative':
+        grad_cam = np.clip(grad_cam,a_min=None, a_max=0)
+    elif apply_clip == 'positive':
+        grad_cam = np.clip(grad_cam, a_min=0,   a_max=None)
+        
+    return grad_cam #same shape as activation layer output
+
+## TODO: layerwise relevance propagation
+## TODO: smoothgrad
